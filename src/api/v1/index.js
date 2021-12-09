96f5e2ba29c46b1ca43b8f69215a771914454a64
Soumosir Dutta <sdutta7@wireless-10-104-233-123.umd.edu>
2021-12-08 19:23:36 -0500

    remove production code

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -14,14 +14,14 @@ app.use(bodyParser.json());
 app.use(blogRoutes);
 // require('./routes/blogRoutes')(app);
 
-if (['production'].includes(process.env.NODE_ENV)) {
-  app.use(express.static('client/build'));
+// if (['production'].includes(process.env.NODE_ENV)) {
+//   app.use(express.static('client/build'));
 
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve('client', 'build', 'index.html'));
-  });
-}
+//   const path = require('path');
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve('client', 'build', 'index.html'));
+//   });
+// }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
