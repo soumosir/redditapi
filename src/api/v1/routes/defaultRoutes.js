b4a53bf9ddd069a1b18498bbf0218db43decd26b
Soumosir Dutta <sdutta7@wireless-10-104-233-123.umd.edu>
2021-12-09 14:24:18 -0500

    Added default route * handler

--- src/api/v1/routes/defaultRoutes.js
+++ src/api/v1/routes/defaultRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express'; 
 const Router = express.Router();
 
-Router.get(async (req, res) => {
+Router.get('*',async (req, res) => {
     res.status(404);
     res.send({'msg':'Requested URL is not served yet!'})
 });
